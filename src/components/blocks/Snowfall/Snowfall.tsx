a2d4e455d2f67decfd1f4dfb1455cc6d45826aa4
empxthy <landshok35@gmail.com>
2024-12-09 23:46:35 +0400

    fixed

--- src/components/blocks/Snowfall/Snowfall.tsx
+++ src/components/blocks/Snowfall/Snowfall.tsx
@@ -2,14 +2,12 @@
 import React from 'react';
 import Snowfall from 'react-snowfall';
 
-const speed= [0.5, 2.0]
 
 const SnowfallEffect: React.FC = () => {
   return (
     <div>
       <Snowfall 
         snowflakeCount={150}
-        speed={speed}
         style={{ position: 'fixed', top: 0, left: 0, zIndex: 9999 }}
       />
     </div>
